3e3caf19c58f381f5ff52e78210cab4f053b8a70
arifoglu <arifoglu2020@gmail.com>
2021-11-19 21:42:34 +0100

    i wrote small js

---
+++ week2/app.js
@@ -0,0 +1,25 @@
+function push()
+{
+  alert(document.getElementById("first").value);
+  document.write(first.value);
+  console.log(first.value);
+}
+
+var mevsim = "soguk" ;
+document.getElementById("winter").innerHTML = mevsim ;
+console.log(typeof mevsim);
+console.log(mevsim);
+
+var derece = 24 ;
+document.getElementById("spring").innerHTML = derece ;
+console.log(typeof derece);
+console.log(derece)
+
+var kiyafet = "tshirt" ;
+document.getElementById("summer").innerHTML = kiyafet ;
+console.log(typeof true)
+console.log(kiyafet)
+
+var portre = null;
+document.getElementById("autumn").innerHTML = portre;
+console.log( typeof portre);
