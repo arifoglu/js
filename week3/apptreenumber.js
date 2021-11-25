105477e8e47d3126b688153ac3335dcc62a8f445
arifoglu <arifoglu2020@gmail.com>
2021-11-25 23:32:22 +0100

    i did  last part of homework

--- week3/apptreenumber.js
+++ week3/apptreenumber.js
@@ -1,21 +1,28 @@
-//when someone entered any 2 numbers for two input areas our function will comparis that two numbers // 
+
+//when someone entered any 2 numbers for two input areas our function will
+//comparis that three numbers // 
+
 function comparis(){
     //first input area and second input area are defined//
     let a = document.getElementById("numero1").value ;
     let b = document.getElementById("numero2").value ;
     let c = document.getElementById("numero3").value ;
-
-
+    
+    
     //comparison options//
-
-
-
-    if(a >= b &&  a >= c && b > c ) 
-    document.getElementById("result").innerHTML = a  + ">" + b + ">" + c;
-    else if(b >= a && b >= c  )
-    document.getElementById("result").innerHTML = b ;
-    else if(c >= a && c >= b )
-    document.getElementById("result").innerHTML = c ;
+    if(a >= b && a >= c && b > c)
+    document.getElementById("result").innerHTML = a + ">" + b + ">" + c ;
+    else if(a >= b && a >= c && c > b)
+    document.getElementById("result").innerHTML = a + ">" + c + ">" + b ;
+    else if(b >= a && b >= c && a > c)
+    document.getElementById("result").innerHTML = b + ">" + a + ">" + c ;
+    else if(b >= a && b >= c && c > a)
+    document.getElementById("result").innerHTML = b + ">" + c + ">" + a;
+    else if(c >= a && c >= b && a > b)
+    document.getElementById("result").innerHTML = c + ">" + a + ">" + b;
+    else if(c >= a && c >= b && b > a)
+    document.getElementById("result").innerHTML = c + ">" + b + ">" + a;
     else
-    document.getElementById("result").innerHTML = "equal" ;
+    document.getElementById("result").innerHTML = "equal"
     }
+    
