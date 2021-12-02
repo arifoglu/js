cfcc3e751f9f2bdae32ba19cfaf910e5c44cf449
arifoglu <arifoglu2020@gmail.com>
2021-12-02 16:01:16 +0100

    i wrote array

---
+++ week4/numerosapp.js
@@ -0,0 +1,42 @@
+
+///////////////////////////////////////////
+////VARIABLE ASSIGN
+
+let numeros = [0,4,9,16,25,36,49,64,81,100];
+let arrayuzunluk = numeros.length;
+let toplam = 0 ;
+let ortalama = toplam / arrayuzunluk ;
+
+/////////////////////////////////////////////
+// SQUARES OF NUMBERS //
+
+for (let i = 0 ; i < arrayuzunluk ; i++){
+    let kareal = Math.pow(numeros[i],2)
+    console.log(kareal)
+}
+///////////////////////////////////////////////
+// SQUARESROOT OF NUMBERS //
+
+for (let i = 0 ; i < arrayuzunluk ; i++){
+    let karekok = Math.sqrt(numeros[i]);
+    console.log(karekok);
+}
+
+////////////////////////////////////////////////
+//SUM OF NUMBERS //
+
+for (let i = 0 ; i < arrayuzunluk ; i++){
+  toplam += numeros[i]
+}
+console.log(toplam)
+
+/////////////////////////////////////////////////
+// MAX and MIN OF NUMBERS
+console.log(Math.min(...numeros))
+console.log(Math.max(...numeros))
+ 
+/////////////////////////////////////////////////
+// AVERAGE OF NUMBERS
+console.log(ortalama)
+
+
