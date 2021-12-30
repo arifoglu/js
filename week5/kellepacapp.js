e6e1c6d4f00bd111cc5d81c31c8a4ba446d9ec82
arifoglu <arifoglu2020@gmail.com>
2021-12-30 18:48:11 +0100

    i added new app

---
+++ week5/kellepacapp.js
@@ -0,0 +1,32 @@
+// Hayvanlarla ilgili array hazirla;
+//array icerisinden hayvan isimlerinden hayvan sayilarini tespit et; 
+// console ile kontrol yap;
+// hayvan listesinden secilen uygun hayvan  turleri icin function yaz;
+//fonksiyonu cagir ;
+//console ile yazdir   ve kontrol et !!!
+
+//hayvanlarin listesini hazirla
+// const  hayvanlar = ["inek","tavuk","domuz","tavuk","koyun","inek","koyun","inek","inek","koyun"];
+
+// //hayvanlarin kac tane oldugunun tespiti console ile kontrolu
+// let inekSayisi = hayvanlar.filter(x => x == "inek").length;
+// let koyunSayisi = hayvanlar.filter(x => x == "koyun").length;
+// let tavukSayisi = hayvanlar.filter(x => x == "domuz").length;
+// let domuzSayisi = hayvanlar.filter(x => x == "tavuk").length;
+ 
+// // const inekbacaksayisi = 
+// console.log("ineksayisi" ,inekSayisi);
+// console.log("koyunsayisi" , koyunSayisi);
+// console.log("domuzsayisi" ,domuzSayisi);
+// console.log("tavuksayisi" , tavukSayisi);
+
+// //kellepaca icin uygun paca sayilarinin tespiti
+// //
+// function pacabul(uygun1,uygun2){
+//     let necipusta = (uygun1 + uygun2)*4 ;
+//     return necipusta ;
+
+// }
+
+// let tespit = pacabul(inekSayisi,koyunSayisi);
+// console.log("kelle paca icin uygun bacak SAyisi" ,tespit);
